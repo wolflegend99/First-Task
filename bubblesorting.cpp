65ea594bfbe451b1ce4d4fe9e63a8ac57259ee83
wolflegend99 <38404778+wolflegend99@users.noreply.github.com>
2018-05-13 22:17:53 +0530

    Sorting Algorithm
    Bubble sort!

---
+++ bubblesorting.cpp
@@ -0,0 +1,36 @@
+// C++ program for Bubble sort
+#include <iostream>
+using namespace std;
+void Swap(int *a, int *b)
+{
+    int temp;
+     temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void Bubblesort(int a[], int n)
+{
+   for (int i = 0; i < n-1; i++)        
+       for (int j = 0; j < n-i-1; j++) 
+           if (a[j] > a[j+1])
+              Swap(&a[j], &a[j+1]);
+}
+void Printarray(int a[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        cout<<a[i]<<" ";
+    cout<<endl;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+      cin>>a[i];
+    Bubblesort(a, n);
+    cout<<"Sorted array"<<endl;
+    Printarray(a, n);
+    return 0;
+}
